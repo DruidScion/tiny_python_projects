ee3284caa09c4cc99783c64e41b03532b6629f6f
DruidScion <mmatkoski14@proton.me>
2025-09-20 17:11:01 -0400

    Complete picnic.py
    - First attempt

--- 03_picnic/picnic.py
+++ 03_picnic/picnic.py
@@ -12,22 +12,19 @@ def get_args():
     """
     parser = argparse.ArgumentParser(description="Picnic game")
     parser.add_argument("food", type=str, nargs="+", help="Item(s) to bring")
-    parser.add_argument(
-        "-s", "--sorted", default=False, metavar="SORT", help="Sort the items"
-    )
+    parser.add_argument("-s", "--sorted", action="store_true", help="Sort the items")
     return parser.parse_args()
 
 
-def format_output(food: str) -> str:
+def format_output(food: list[str]) -> str:
     """
     Format the string properly for output
     """
     if len(food) == 1:
         return food[0]
-    elif len(food) == 2:
+    if len(food) == 2:
         return " and ".join(food)
-    else:
-        return ", ".join(food[:-1]) + ", and " + food[-1]
+    return ", ".join(food[:-1]) + ", and " + food[-1]
 
 
 def main():
@@ -35,7 +32,10 @@ def main():
     Main Function
     """
     args = get_args()
-    food = args.food
+    if args.sorted:
+        food = sorted(args.food)
+    else:
+        food = args.food
     print(f"You are bringing {format_output(food)}.")
 
 
