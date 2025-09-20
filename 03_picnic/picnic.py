c3857a56eb98ae15b3deef003a981987e4860bdc
DruidScion <mmatkoski14@proton.me>
2025-09-19 22:58:38 -0400

    Add picnic.py
    - Add boilerplate

---
+++ 03_picnic/picnic.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Picnic
+"""
+
+import argparse
+
+
+def get_args():
+    """
+    Parse Command-line Arguments
+    """
+    parser = argparse.ArgumentParser(description="XXXXXX")
+    parser.add_argument("xxx", help="xxxxx")
+    return parser.parse_args()
+
+
+def main():
+    """
+    Main Function
+    """
+    ...
+
+
+if __name__ == "__main__":
+    main()
