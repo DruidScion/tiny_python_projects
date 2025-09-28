5a2b21bc44a60aa7a128a922c676b7b509329463
DruidScion <mmatkoski14@proton.me>
2025-09-27 21:26:24 -0400

    Write boilerplate

---
+++ 05_howler/howler.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Howler
+"""
+
+import argparse
+
+
+# ---------------------------------------------------------------
+def get_args():
+    """Parse command-line arguments"""
+    parser = argparse.ArgumentParser(description="Howler")
+    return parser.parse_args()
+
+
+# ---------------------------------------------------------------
+def main():
+    args = get_args()
+    pass
+
+
+if __name__ == "__main__":
+    main()
