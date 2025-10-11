656429f8ef301c4ed105d4371a61956a52596f72
DruidScion <mmatkoski14@proton.me>
2025-10-11 19:09:53 -0400

    Finish boilerplate
    - Basic string input to capitalize output

--- 05_howler/howler.py
+++ 05_howler/howler.py
@@ -10,13 +10,15 @@ import argparse
 def get_args():
     """Parse command-line arguments"""
     parser = argparse.ArgumentParser(description="Howler")
+    parser.add_argument("text", metavar="str", nargs="*", help="Input text")
     return parser.parse_args()
 
 
 # ---------------------------------------------------------------
 def main():
     args = get_args()
-    pass
+    yell = " ".join(args.text).upper()
+    print(yell)
 
 
 if __name__ == "__main__":
