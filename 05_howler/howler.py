a2de9ace9ad0b7b25d91dc7129f42f71fc9a61dc
DruidScion <mmatkoski14@proton.me>
2025-10-11 20:38:36 -0400

    feat: process input file
    - test if argument is file
    - if file, return contents as capitalized string

--- 05_howler/howler.py
+++ 05_howler/howler.py
@@ -4,6 +4,7 @@ Howler
 """
 
 import argparse
+import os
 
 
 # ---------------------------------------------------------------
@@ -15,10 +16,27 @@ def get_args():
 
 
 # ---------------------------------------------------------------
+def in_file():
+    """Process if the argument is a file"""
+    args = get_args()
+    file_text = " ".join(args.text)
+    if os.path.isfile(file_text):
+        with open(file_text, "rt", encoding="utf-8") as file:
+            for text in file:
+                file_text = "".join(text).upper()
+        return file_text
+    return None
+
+
+# ---------------------------------------------------------------
 def main():
+    """Main function"""
+    file_text = in_file()
     args = get_args()
     yell = " ".join(args.text).upper()
-    print(yell)
+    if file_text is None:
+        print(yell)
+    print(file_text)
 
 
 if __name__ == "__main__":
